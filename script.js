c9192347e05fb1fca2c1cc58ed823740c6c495f5
Rohitilak <rohittilak086@gmail.com>
2024-01-13 18:37:55 +0530

    dark mode project

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".circle-container").addEventListener("click", () => {
+    document.getElementById("body").classList.toggle("dark-mode");
+    document.querySelector(".circle-btn").classList.toggle("addLeft");
+  });
